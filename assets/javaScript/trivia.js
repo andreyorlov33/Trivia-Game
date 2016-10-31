df6abaa96c48d788538d187e20f7f3717e82da34
Andrey Orlov <andreyorlov33@gmail.com>
2016-10-30 17:45:51 -0700

    final commit

--- assets/javaScript/trivia.js
+++ assets/javaScript/trivia.js
@@ -1,3 +1,93 @@
+$(document).on("click", '#startGame', startGame);
+
+$(document).on("click", '#reset', function(){
+	$('.text').remove();
+	$('#reset').remove();
+	clearScore();
+	clearInterval(counter);
+	correctAnswersCount = 0;
+	wrongAnswersCount = 0;s
+	shotClockViolation = 0;
+	totalNumberOfQuestions = 0
+	imageDisplay;
+	gameStatus;
+	counter; 
+	inGame = true;
+	pickRandomQuestion();	
+});
+
+
+
+var  data={
+ 	"questions":[
+ 	{
+		"question": "This Player is the Los Angeles Lakers All Time Leading Scorer",
+		"answers": ["Kareem Abdul-Jabbars","Kobe Bryant", "Elgin Baylo","Jerry West"],
+		"correctAnswer" : 1,
+		"imageUrl": "assets/images/kobe.gif",
+	},
+	{
+		"question": "This Player is the New York Knicks All Time Leading Scorer",
+		"answers": ["John Starks","Patrick Ewing", "Carmelo Anthony","Walt Frazier"],
+		"correctAnswer" : 1,
+		"imageUrl": "assets/images/ewing.gif",
+	},
+
+	{
+		"question": "This Player is the Milwakee Bucks All Time Leading Scorer",
+		"answers": ["Ray Allen", "Glenn Robinson", "Kareem Abdul-Jabbar", "Michael Redd"],
+		"correctAnswer": 2,
+		"imageUrl": "assets/images/Kareem.gif",
+	},
+
+
+	{
+		"question": "This Player is the Portland Trail Blazers All Time Leading Scorer",
+		"answers": ["Lamarcus Aldridge", "Terry Porter", "Clifford Robinson","Clyde Drexler"],
+		"correctAnswer": 3,
+		"imageUrl": "assets/images/clyde.gif",
+	},
+	{
+		"question": "This Player is the Miami Heat All Time Leading Scorer",
+		"answers": ["Glen Rice","Alonzo Morning", "Tim Hardaway","Dwyane Wade"],
+		"correctAnswer" : 3,
+		"imageUrl": "assets/images/wade.gif",
+	},
+	{
+		"question": "This Player is the Golden State Warriors All Time Leading Scorer",
+		"answers": ["Rick Barry","Chris Mullin", "Wilt Chamberlain","Stephen Curry"],
+		"correctAnswer" : 2,
+		"imageUrl": "assets/images/wilt.gif",
+	},
+	{
+		"question": "This Player is the Minisota Timber Wolves All Time Leading Scorer",
+		"answers": ["Sam Mitchell", "Kevin Love", "Wally Szczerbiak","Kevin Garnett"],
+		"correctAnswer" : 3,
+		"imageUrl": "assets/images/kg.gif",
+	},
+	{
+		"question": "This Player is the Charlotte Hornets All Time Leading Scorer",
+		"answers": ["Dell Curry", "	Gerald Wallace", "Larry Johnson","Kemba Walker"],
+		"correctAnswer" : 0,
+		"imageUrl": "assets/images/dell.gif",
+	},
+	{
+		"question": "This Player is the Indiana Pacers All Time Leading Scorer",
+		"answers": ["Billy Knight", "Jermaine O'Neal", "Reggie Miller","Rik Smiths"],
+		"correctAnswer" : 2,
+		"imageUrl": "assets/images/reggie.gif",
+	},
+]
+}
+
+function pushData(){
+	for(var i=0; i <data.questions.length; i++){
+		console.log(data.questions[i]);
+		questionsArray.push(data.questions[i]);
+		console.log(questionsArray)
+	}
+}
+
 // VARIABLES //
 
 var correctAnswersCount = 0;
@@ -6,49 +96,23 @@ var shotClockViolation = 0;
 var totalNumberOfQuestions = 0
 var imageDisplay;
 var gameStatus;
-var count; 
-
-var questionOne = {
-	question: "This Player is the New York Knicks All Time Leading Scorer",
-	answers: ["John Starks","Patrick Ewing", "Carmelo Anthony","Walt Frazier"],
-	correctAnswer : 1,
-	imageUrl: 'assets/images/ewing.gif',
-
-}
-
-var questionTwo = {
-	question: "This Player is the Milwakee Bucks All Time Leading Scorer",
-	answers: ["Ray Allen", "Glenn Robinson", "Kareem Abdul-Jabbar", "Michael Redd"],
-	correctAnswer: 2,
-	imageUrl: 'assets/images/Kareem.gif',
-}
-
-var questionThree = {
-	question: "This Player is the Portland Trail Blazers All Time Leading Scorer",
-	answers: ["Lamarcus Aldridge", "Terry Porter", "Clifford Robinson","Clyde Drexler"],
-	correctAnswer: 3,
-	imageUrl: 'assets/images/clyde.gif',
-}
-
-var ajaxUrl = "assets/";
+var counter; 
+var inGame;
 
 
-
-var questionsArray = [questionOne, questionTwo , questionThree]
+var questionsArray = [];
 
 
 //GAME INITIALIZATION AND DOM DISPLAY FUNCTIONS //
 
 function pickRandomQuestion(){
-	
-	
-	$("#wrongBanner").remove();
-	$("#correctBanner").remove();
-	$(".playerImage").remove();
-	$(".question").empty();
-	$(".answer").remove();
-	var randomQuestion = questionsArray[Math.floor(Math.random()*questionsArray.length)];
+	clearScore();
+	if(inGame === false) return;
+	var randomIndex = Math.floor(Math.random()*questionsArray.length);
+	var randomQuestion = questionsArray[randomIndex];
+	questionsArray.splice(randomIndex, 1);
 	displayQuestion(randomQuestion);
+	console.log(randomQuestion);
 	totalNumberOfQuestions ++;
 	runShotClock();
 }
@@ -63,14 +127,14 @@ function displayQuestion(questionDisplayed){
 	for(var i=0; i<questionDisplayed.answers.length; i++){
 		var a = questionDisplayed.answers[i];
 		if(a == correctDisplay){
-			var b = '<div class="answer correct"><h3>'+questionDisplayed.answers[i]+'</h3></div>';
+			var b = '<div class="answer correct"><h2>'+questionDisplayed.answers[i]+'</h2></div>';
 			
 		}else{
-			var b = '<div class="answer" ><h3>'+questionDisplayed.answers[i]+'</h3></div>';
+			var b = '<div class="answer" ><h2>'+questionDisplayed.answers[i]+'</h2></div>';
 		}
 			$(".container").append(b);
 	}
-	$(".question").html('<h2>"'+questionText+'"</h2>');
+	$(".container").prepend('<div class="question"> <br><br><h2>"'+questionText+'"</h2></div>');
 	$(".container").append(imageDisplay);
 	setAnswerEventClicker(correctDisplay);
 }
@@ -82,15 +146,14 @@ function setAnswerEventClicker(correctDisplay){
 			console.log("click");
 			correctAnswerClickHandler(correctDisplay);
 			correctAnswersCount ++;
-			
+			showScore(correctAnswersCount, wrongAnswersCount);
 		}else{
 			wrongAnswersCount ++;
 			wrongAnswerClickHandler(correctDisplay);
-			
+			showScore(correctAnswersCount, wrongAnswersCount);
 		}	
 	});
-	showScore(correctAnswersCount, wrongAnswersCount);
-	checkScore();
+	
 }
 
 
@@ -100,8 +163,7 @@ function correctAnswerClickHandler(correctDisplay){
 	$('<h2 id="correctBanner"> Nice Shot!! '+correctDisplay+' Is Correct!!!</h2>').insertAfter(".question");
 	$(imageDisplay).removeClass("hidden");
 	stopShotClock();
-	setTimeout(pickRandomQuestion, 5000);
-	
+	checkScore();
 }
 
 function wrongAnswerClickHandler(correctDisplay){
@@ -109,44 +171,43 @@ function wrongAnswerClickHandler(correctDisplay){
 	$('<h2 id="wrongBanner">You Missed ... <br> correctAnswer is: '+correctDisplay+'</h2>').insertAfter(".question");
 	$(imageDisplay).removeClass("hidden");
 	stopShotClock();
-	setTimeout(pickRandomQuestion, 5000);	
+	checkScore();
 }
 
 // NEW RANDOM QUSTION//
 
 function checkScore(){
-	if(wrongAnswersCount === 1){
-		youLost();
+
+	if(wrongAnswersCount === 10){
+		inGame = false;
+		var percentage = (correctAnswersCount/totalNumberOfQuestions).toFixed(2);
+		$('.container').empty();
+		var text = '<h2 class="text">Click on the ball to play again!</h2>';
+		var startOverButton = '<div class="basketball" id="reset">';
+		var youWonMessage = '<h2 class="text">Nice shooting! You won!... <br> You shot '+correctAnswersCount+' out of '+totalNumberOfQuestions+' <br> Your field goal percentage is '+percentage+' and you had '+shotClockViolation+' turn overs!</h2>';
+		$('.container').append(youWonMessage, startOverButton, text);
+}
+
+	else if(correctAnswersCount === 5){
+		inGame = false;
+		var percentage = Math.floor(correctAnswersCount/totalNumberOfQuestions);
+		$('.container').empty();
+		var text = '<h2 class="text">Click on the ball to play again!</h2>';
+		var startOverButton = '<div class="basketball" id="reset">';
+		var youLostMessage = '<h2 class="text">Nice try but you lost... <br> You shot '+correctAnswersCount+' out of '+totalNumberOfQuestions+' <br> Your field goal percentage is '+percentage+' and you had '+shotClockViolation+' turn overs!</h2>';
+		$('.container').append(youLostMessage, startOverButton, text);
+	}	
+	else{
+
+		setTimeout(pickRandomQuestion, 5000);
 	}
-	else if(correctAnswersCount === 20){
-		youWon();
-	}
-}
-function youWon(){
-	$('.container').empty();
-	var text = "<h2>Click on the ball to play again!</h2>";
-	var startOverButton = '<div class="basketball">';
-	var youWonMessage = '<h2>Nice shooting! You won!... <br> You shot '+correctAnswersCount+' out of '+totalNumberOfQuestions+' <br> Your field goal percentage is '+correctAnswersCount/totalNumberOfQuestions+' and you had '+shotClockViolation+' turn overs!</h2>';
-	$('.container').append(youWonMessage, startOverButton, text);
-
-
 }
 
-function youLost(){
-	$('.container').empty();
-	var text = "<h2>Click on the ball to play again!</h2>";
-	var startOverButton = '<div class="basketball">';
-	var youLostMessage = '<h2>Nice try but you lost... <br> You shot '+correctAnswersCount+' out of '+totalNumberOfQuestions+' <br> Your field goal percentage is '+correctAnswersCount/totalNumberOfQuestions+' and you had '+shotClockViolation+' turn overs!</h2>';
-	$('.container').append(youLostMessage, startOverButton, text);
-
-
-
-}
 
 // SHOT CLOCK AND SCORE FUNCTIONS// 
 function runShotClock(){
 	shotClock = 25;
-     counter = setInterval(decrement, 1000);
+    counter = setInterval(decrement, 1000);
 }
 
 function stopShotClock(){
@@ -165,8 +226,9 @@ function decrement(){
 function timeIsUp(){
  	$('.shotClock').html('<h2>00</h2>');
  	shotClockViolation ++;
-	 $(".answer").remove();
+	$(".answer").remove();
  	$('<h2 id="wrongBanner">You are out of time! <br> Try again!</h2>').insertAfter(".question");
+ 	clearInterval(counter);
 	setTimeout(pickRandomQuestion, 5000);	
 }
 
@@ -176,21 +238,34 @@ function showScore(home,away){
 }
 
 function startGame (){
+	pushData();
+	clearScore();
+	clearInterval(counter);
 	$(".welcome").addClass("hidden");
 	$(".shotClock").removeClass("hidden");
 	$(".container").removeClass("hidden");
 	correctAnswersCount = 0;
 	wrongAnswersCount = 0;
 	shotClockViolation = 0;
-	pickRandomQuestion();
+	totalNumberOfQuestions = 0
+	imageDisplay;
+	gameStatus;
+	counter; 
+	inGame = true;
+	pickRandomQuestion();	
+	pushData;
+
+	
+	
 }
 
 
-
-
-$(document).ready(function(){
-	$(".basketball").on("click", startGame);
-	$(".answer").on("click", stopShotClock);	
-});
+function clearScore(){
+	$(".question").remove();
+	$(".answer").remove();
+	$(imageDisplay).addClass("hidden");
+$("#correctBanner").remove();
+$("#wrongBanner").remove();
+}
 
 
